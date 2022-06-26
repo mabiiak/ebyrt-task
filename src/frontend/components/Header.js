a172f83e73a8974796dace0d8106c3521debf0de
mabiiak <mabiane.polniak@gmail.com>
2022-06-26 00:53:48 -0300

    feat: implements component header

--- src/frontend/components/Header.js
+++ src/frontend/components/Header.js
@@ -4,6 +4,15 @@ class Header extends Component {
   render() {
     return (
       <header>
+        <h1>E B Y R T - T A S K S</h1>
+        <div>
+          <h3>
+            O R D E R
+          </h3>
+          <button>D A T E</button>
+          <button>N A M E</button>
+          <button>S T A T U S</button>
+        </div>
       </header>
     )
   }
