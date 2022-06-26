be5614bc8eb93acf67d3bf7708cec5a8476fa0b3
mabiiak <mabiane.polniak@gmail.com>
2022-06-26 01:13:13 -0300

    feat: implements component create task

--- src/frontend/components/Create.js
+++ src/frontend/components/Create.js
@@ -1,9 +1,47 @@
 import React, { Component } from 'react';
 
 class Create extends Component {
+  constructor() {
+    super();
+
+    this.handleChange = this.handleChange.bind(this);
+    this.createButton = this.createButton.bind(this);
+
+    this.state = {
+      title: '',
+      description: '',
+    }
+  }
+
+  handleChange({ target }) {
+    const { value, name } = target;
+    this.setState({ [name]: value });
+  }
+
+  createButton() {
+    const { title, description } = this.state;
+    console.log( title, description);
+  }
+
   render() {
     return (
       <>
+        <h3>
+          N E W T A S K
+        </h3>
+        <input
+          type='text'
+          name='title'
+          onChange={ this.handleChange }
+          placeholder='T I T L E'
+        />
+        <input
+          type='text'
+          name='description'
+          onChange={ this.handleChange }
+          placeholder='D E S C R I P T I O N'
+        />
+        <button onClick={ this.createButton }>C R E A T E</button>
       </>
     )
   }
