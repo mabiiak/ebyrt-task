97a004302af05c59d841d1ad577520a8560a3b2b
mabiiak <mabiane.polniak@gmail.com>
2022-06-26 01:27:34 -0300

    feat: implements component model task card

--- src/frontend/components/TaskCard.js
+++ src/frontend/components/TaskCard.js
@@ -4,6 +4,25 @@ class TaskCard extends Component {
   render() {
     return (
       <>
+        <div>
+          <h3>T I T L E</h3>
+          <h4>2 5 - 0 6 - 2 2</h4>
+        </div>
+        <div>
+          <button>E D I T A R</button>
+          <button>E X C L U I R</button>
+        </div>
+        <p>
+          D E S C R I C A O
+        </p>
+        <div>
+          <input type='radio' name='status' id='pendente'/>
+          <label for='pendente'>P E N D E N T E</label>
+          <input type='radio' name='status' id='andamento'/>
+          <label for='andamento'>E M A N D A M E N T O</label>
+          <input type='radio' name='status' id='Concluído'/>
+          <label for='Concluído'>C O N C L U I D O</label>
+        </div>
       </>
     )
   }
