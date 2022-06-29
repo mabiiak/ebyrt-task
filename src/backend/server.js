671bed0e3509b9c58c8a51be96bf49b0e6955ad9
mabiiak <mabiane.polniak@gmail.com>
2022-06-29 00:05:22 -0300

    fix: server

--- src/backend/server.js
+++ src/backend/server.js
@@ -1,7 +1,11 @@
-const express = require('express');
+import routeTasks from './routes/routeTasks.js'
+import express from 'express';
+
 const app = express();
 
 app.use(express.json());
+
+app.use('/tasks', routeTasks);
 
 app.listen(3001, function () {
   console.log('Example app listening on port 3001!');
