467789f9b4feecc874f1f1d2c82751cb31407585
mabiiak <mabiane.polniak@gmail.com>
2022-07-02 15:08:02 -0300

    refactor: change checkbox radius for select

--- src/frontend/components/RenderCard.js
+++ src/frontend/components/RenderCard.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
 import { Task } from '../styles/cardTask';
+import Trash from '../images/lixeira.png';
+import Edit from '../images/editar.png';
 
 class RenderCard extends Component {
   constructor() {
@@ -8,8 +10,8 @@ class RenderCard extends Component {
   }
 
   isCheck({ target }) {
-    const { id } = target;
-    console.log(id);
+    const { value } = target;
+    console.log(value);
   }
 
   render() {
@@ -22,38 +24,18 @@ class RenderCard extends Component {
               <h3>{ task.title }</h3>
               <h5>{ task.date }</h5>
             </div>
-            <div id='options'>
-              <label for='pendente'>
-                <input
-                type='radio'
-                name='status'
-                id='pendente'
-                onChange={ this.isCheck }
-              />
-                Pendente
-              </label>
-              <label for='andamento'>
-                <input
-                type='radio'
-                name='status'
-                id='andamento'
-                onChange={ this.isCheck }
-              />
-                Em andamento
-              </label>
-              <label for='Concluído'>
-                <input
-                type='radio'
-                name='status'
-                id='Concluído'
-                onChange={ this.isCheck }
-              />
-                concluído
-              </label>
-            </div>
-            <div id='buttons'>
-              <button>EDITAR</button>
-              <button>DELETAR</button>
+            <div id='second-line'>
+              <select onChange={ this.isCheck }>
+                <option value='pendente'>Pendente</option>
+                <option value='andamento'>Em andamento</option>
+                <option value='concluído'>concluído</option>
+              </select>
+              <button>
+                <img src={ Trash } alt='icon trash'/>
+              </button>
+              <button>
+                <img src={ Edit } alt='icon trash'/>
+              </button>
             </div>
           </Task>
         ))
